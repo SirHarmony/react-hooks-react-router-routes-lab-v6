a5a046156e0d20a135a94ea16f6495c5f249a631
Harmony <harmonymukolwe@gmail.com>
2024-08-14 16:41:48 +0300

    Implements app routing

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actorsData, setActorsData] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/actors`)
+      .then((res) => res.json())
+      .then((data) => setActorsData(data));
+  });
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        <article>
+          {actorsData.map((actorData) => {
+            return (
+              <div key={actorData.id}>
+                <h2>{actorData.name}</h2>
+                <ul>
+                  {actorData.movies.map((movie) => (
+                    <li>{movie}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </article>
       </main>
     </>
   );
-};
+}
 
 export default Actors;
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directorsData, setDirectorsData] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/directors`)
+      .then((res) => res.json())
+      .then((data) => setDirectorsData(data));
+  });
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        <article>
+          {directorsData.map((directorData) => {
+            return (
+              <div key={directorData.id}>
+                <h2>{directorData.name}</h2>
+                <ul>
+                  {directorData.movies.map((movie) => (
+                    <li>{movie}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </article>
       </main>
     </>
   );
-};
+}
 
 export default Directors;
